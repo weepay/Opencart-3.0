5dd4fd64565d01977c812d4ec72545d9034c824d
weepay <49822776+weepay@users.noreply.github.com>
2019-10-01 14:28:45 +0300

    Update weepay_payment.php

--- catalog/model/extension/payment/weepay_payment.php
+++ catalog/model/extension/payment/weepay_payment.php
@@ -5,7 +5,7 @@ class ModelExtensionPaymentWeepayPayment extends Model {
     public function getMethod($address, $total) {
         $this->load->language('extension/payment/weepay_payment');
 
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get('moka_payment_geo_zone_id') . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int) $this->config->get('weepay_payment_geo_zone_id') . "' AND country_id = '" . (int) $address['country_id'] . "' AND (zone_id = '" . (int) $address['zone_id'] . "' OR zone_id = '0')");
 
         if ($this->config->get('weepay_total') > 0 && $this->config->get('weepay_payment_total') > $total) {
             $status = false;
