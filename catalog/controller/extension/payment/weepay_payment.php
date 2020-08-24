43506db33515984f26603d388b6a149b9777947e
weepay <49822776+weepay@users.noreply.github.com>
2020-08-24 13:16:18 +0300

    Update weepay_payment.php

--- catalog/controller/extension/payment/weepay_payment.php
+++ catalog/controller/extension/payment/weepay_payment.php
@@ -52,7 +52,7 @@ class ControllerExtensionPaymentWeepayPayment extends Controller
     public function checkoutform()
     {
         try {
-            $setCookie = $this->setcookieSameSite("PHPSESSID", $_COOKIE['PHPSESSID'], time() + 86400, "/", $_SERVER['SERVER_NAME'], 0, 0);
+            $setCookie = $this->setcookieSameSite("PHPSESSID", $_COOKIE['PHPSESSID'], time() + 86400, "/", $_SERVER['SERVER_NAME'], true, true);
             $data['checkout_form_content'] = '';
             $data['error'] = '';
             $data['form_class'] = $this->config->get('payment_weepay_payment_form_type');
